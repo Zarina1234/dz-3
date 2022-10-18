c9d16f05109b679796a8d912de14a580ceee6553
Zarina Nazimidinova <zarinanazimidinova229@gmail.com>
2022-10-18 12:24:24 +0600

    dz-3

---
+++ main.js
@@ -0,0 +1,49 @@
+// // lessson-3 dz-1
+// function title1 () {
+//     const number = prompt('ведите число')
+// const numbers = prompt('ведите число')
+
+// if(number < numbers) {
+//     alert(number + " " +'меньше')
+// }
+// else if(number ===  numbers) {
+//     alert ('равно')
+// }
+// else{
+//     alert (number + " " + 'меньше')
+
+
+
+// }
+// }
+// title1()
+
+
+
+//dz-2
+
+// const a = [1,2,3]
+// const b = [4,3,2,1]
+
+// function twoMass(a,b) {
+//     if(a.length > b.length)  {
+//         console.log(a.length + "больше");
+//     }
+    
+//     else if(a.length < b.length) {
+//        console.log(b.length + " " + 'больше'); 
+//     }
+// }
+//  twoMass(a,b)
+
+
+//dz-3
+
+function countChar() {
+    let x = 'countChar';
+    console.log( x.length );
+}
+    countChar()
+
+
+
